db589cac427acba58c5c1f2ecc766192830f8131
Eldar Omuraliev <eldar.omuraliev@livingsocial.com>
2014-11-15 13:27:31 -0500

    only one global angular object should be created

--- test/loader_spec.js
+++ test/loader_spec.js
@@ -3,8 +3,20 @@
 'use strict';
 
 describe('setupModuleLoader', function() {
-  it ('exposes angular on the window', function() {
+  beforeEach(function() {
+    delete window.angular;
+  });
+
+  it('exposes angular on the window', function() {
     setupModuleLoader(window);
     expect(window.angular).toBeDefined();
   });
+
+  it('creates angular just once', function() {
+    setupModuleLoader(window);
+    var ng = window.angular;
+    setupModuleLoader(window);
+    expect(window.angular).toBe(ng);
+  });
+
 });
